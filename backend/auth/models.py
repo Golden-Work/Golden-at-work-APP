a118ece29ab8fd6a6b694a2ccd8f54b16f74c97c
Gylg4m3sh <stgarciam@unal.edu.co>
2023-09-28 16:36:53 -0500

    Modelos usuarios admin/superusuario e impelemtnos
    "GA-39" & "GA-16"

--- backend/auth/models.py
+++ backend/auth/models.py
@@ -1,3 +1,73 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 
-# Create your models here.
+# Administrador para gestionar usuarios y administradores
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('El correo electrónico es obligatorio')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Los superusuarios deben tener is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Los superusuarios deben tener is_superuser=True.')
+
+        return self.create_user(email, password, **extra_fields)
+
+# Modelo usuarios y administradores
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(unique=True, blank=False, null=False)
+    first_name = models.CharField(max_length=30, blank=False, null=False)
+    last_name = models.CharField(max_length=30, blank=False, null=False)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    password = models.CharField(max_length=128, blank=False, null=False)
+
+    
+    USERNAME_FIELD = 'email'
+    
+    REQUIRED_FIELDS = ['first_name', 'last_name','email','password']
+
+  
+    objects = CustomUserManager()
+
+    def get_full_name(self):
+        return f'{self.first_name} {self.last_name}'
+
+    def get_short_name(self):
+        return self.first_name
+
+    def has_perm(self, perm, obj=None):
+        return self.is_superuser
+
+    def has_module_perms(self, app_label):
+        return self.is_superuser
+    
+
+    # Modelo implementos
+class Implement(models.Model):
+    name = models.CharField(max_length=255, blank=False, null=False)
+    description = models.TextField(blank=False, null=False)
+    available = models.BooleanField(default=True)
+    borrowed_by = models.ForeignKey('User', null=True, blank=True, on_delete=models.SET_NULL)
+    borrow_date = models.DateTimeField(null=True, blank=True)
+    return_date = models.DateTimeField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+    
+
+
+
+
+    
