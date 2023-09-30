a744362078cc564282da2e28c54ce02b328b3dba
Sebastián Castañeda <s.castaneda@3lupz.com>
2023-09-29 22:31:01 -0500

    add password validation

--- backend/back_auth/serializers.py
+++ backend/back_auth/serializers.py
@@ -10,3 +10,25 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = '__all__'
+
+    def validate_password(self, value):
+        # validate that the password is at least 8 characters long, and has at least 1 digit and 1 uppercase letter, and 1 lowercase letter
+        errors = []
+        if len(value) < 8:
+            errors.append('La contraseña debe tener al menos 8 caracteres')
+
+        if not any(char.isdigit() for char in value):
+            errors.append('La contraseña debe tener al menos 1 número')
+
+        if not any(char.isupper() for char in value):
+            errors.append(
+                'La contraseña debe tener al menos 1 letra mayúscula')
+
+        if not any(char.islower() for char in value):
+            errors.append(
+                'La contraseña debe tener al menos 1 letra minúscula')
+
+        if errors:
+            raise serializers.ValidationError(errors)
+
+        return value
