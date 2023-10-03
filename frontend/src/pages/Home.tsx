6ee4e8caa5e0b20e433cafab8e3ec144dbb20ab1
Omar Nicolas Guerrero Guerrero <oguerrero@unal.edu.co>
2023-10-03 05:09:11 -0500

    GA-64 retoque barra de scroll

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -3,9 +3,9 @@ import logo from "../assets/Imagenes/Logo-placejolder.png";
 import Boton from "../Componentes/Boton";
 import BarraDeBusqueda from "../Componentes/BarraDeBusqueda";
 import ImagenDeScroll from "../Componentes/ImagenDeScroll";
-import '../Hojas-de-estilo/Home.css'
+import "../Hojas-de-estilo/Home.css";
 
-// div>p.parrafo-*7>div*4 
+// div>p.parrafo-*7>div*4
 
 function Home() {
   //const [count, setCount] = useState(0)
@@ -79,13 +79,42 @@ function Home() {
 
       <div className="contenedor-principal">
         <div className="Contenedor-de-scrol-de-imagenes">
-          <ImagenDeScroll nombreDelObjeto="mesa" imagen={1} texto="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore" />
+          <ImagenDeScroll
+            nombreDelObjeto="mesa"
+            imagen={1}
+            texto="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore"
+          />
 
-          <ImagenDeScroll nombreDelObjeto="Raqueta" imagen={2} texto="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore" />
+          <ImagenDeScroll
+            nombreDelObjeto="Raqueta"
+            imagen={2}
+            texto="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore"
+          />
 
-          <ImagenDeScroll nombreDelObjeto="Ajedrez" imagen={3} texto="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore" />
+          <ImagenDeScroll
+            nombreDelObjeto="Ajedrez"
+            imagen={3}
+            texto="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore"
+          />
 
-          <ImagenDeScroll nombreDelObjeto="futbolito" imagen={4} texto="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore" />
+          <ImagenDeScroll
+            nombreDelObjeto="futbolito"
+            imagen={4}
+            texto="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore"
+          />
+
+          <ImagenDeScroll
+            nombreDelObjeto="Parques"
+            imagen={5}
+            texto="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore"
+          />
+
+          <ImagenDeScroll
+            nombreDelObjeto="Pelota"
+            imagen={6}
+            texto="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore"
+          />
+
         </div>
         <div className="contenedor-de-parte-baja">
           <div className="tabla"></div>
