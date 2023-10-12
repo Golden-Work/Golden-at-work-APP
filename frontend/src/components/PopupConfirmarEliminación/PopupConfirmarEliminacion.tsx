be6dc8980cdf9c5479100fdb73d0cdb2ec3b0d58
Sebastián Castañeda <s.castaneda@3lupz.com>
2023-10-12 17:51:07 -0500

    fix

--- frontend/src/components/PopupConfirmarEliminación/PopupConfirmarEliminacion.tsx
+++ frontend/src/components/PopupConfirmarEliminación/PopupConfirmarEliminacion.tsx
@@ -2,35 +2,36 @@ import classes from "./PopupConfirmarEliminacion.module.css"
 import useDeleteUser from "@/hooks/useDeleteUser"
 import * as React from "react"
 
-interface propsProps{
-    visible: boolean,
-    onClose: () => void
+interface propsProps {
+  visible: boolean
+  onClose: () => void
 }
 
+function MyModal({ visible, onClose }: propsProps) {
+  const [_, setAnchorEl] = React.useState<null | HTMLElement>(null)
+  const { deleteUser } = useDeleteUser()
+  const cancelarOnclose = () => {
+    setAnchorEl(null)
+  }
 
-function MyModal({visible, onClose}: propsProps){
-    const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null)
-    const {
-        deleteUser
-    } = useDeleteUser()
-    const cancelarOnclose = () =>{
-        setAnchorEl(null);
-    }
-
-    if(!visible) return null
-    return(
-        <div onClick={onClose} className={classes.popup}>
-            <div className={classes.ventana}>
-                <div className={classes.formventana}>
-                    <p>{'¿Está seguro que desea eliminar su cuenta?'}</p>
-                    <div className={classes.botones}>
-                        <button className={classes.confirmar} onClick={deleteUser}>Confirmar</button>
-                        <button className={classes.cancelar} onClick={cancelarOnclose}>Cancelar</button>
-                    </div>
-                </div>
-            </div>
+  if (!visible) return null
+  return (
+    <div onClick={onClose} className={classes.popup}>
+      <div className={classes.ventana}>
+        <div className={classes.formventana}>
+          <p>{"¿Está seguro que desea eliminar su cuenta?"}</p>
+          <div className={classes.botones}>
+            <button className={classes.confirmar} onClick={deleteUser}>
+              Confirmar
+            </button>
+            <button className={classes.cancelar} onClick={cancelarOnclose}>
+              Cancelar
+            </button>
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  )
 }
 
 export default MyModal
