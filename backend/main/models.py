b7cf5f80ad32a0e3bdf1d2c2e44d913b927e0918
Omar Nicolas Guerrero Guerrero <oguerrero@unal.edu.co>
2023-11-01 18:01:37 -0500

    correccion main->model

--- backend/main/models.py
+++ backend/main/models.py
@@ -15,7 +15,7 @@ class Implement(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField(blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
-    image = models.ImageField(upload_to='   /', blank=True)
+    image = models.ImageField(upload_to='implements/', blank=True)
     def __str__(self):
         return self.name
 
