f0d3788d553c4e46c0ce3fb5740c33fa780c2c95
Omar Nicolas Guerrero Guerrero <oguerrero@unal.edu.co>
2023-11-02 19:07:02 -0500

    correccion de superpocicion de archivos en el front

--- frontend/src/api/index.ts
+++ frontend/src/api/index.ts
@@ -1,10 +1,7 @@
-import { QueryClient } from "@tanstack/react-query"
 import axios from "axios"
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_APP_BASE_URL as string,
 })
-
-export const queryClient = new QueryClient()
 
 export default api
