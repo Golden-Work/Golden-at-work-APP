25ccab35e731dffdba3158d6cc3f64b1b99c7e96
Sebastián Castañeda <s.castaneda@3lupz.com>
2023-11-02 20:25:07 -0500

    fix

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,20 @@
 import "react-toastify/dist/ReactToastify.css"
 import { ToastContainer } from "react-toastify"
 import ThemeContextProvider from "./contexts/MUIContext"
-
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import { RouterProvider } from "react-router-dom"
 import router from "./router"
 
+const queryClient = new QueryClient()
+
 function App() {
   return (
-    <ThemeContextProvider>
-      <ToastContainer />
-      <RouterProvider router={router} />
-    </ThemeContextProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeContextProvider>
+        <ToastContainer />
+        <RouterProvider router={router} />
+      </ThemeContextProvider>
+    </QueryClientProvider>
   )
 }
 
