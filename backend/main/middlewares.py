532889f92ce173027926e4d07a97ddbdf54c0e4c
Sebastián Castañeda <sebastiansnipez@gmail.com>
2023-11-19 19:43:40 -0500

    modify middleware to handle images

--- backend/main/middlewares.py
+++ backend/main/middlewares.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.models import User
 from .models import LogInfo
-
+import json
 
 """
 This middleware logs information about the requests made to the API (not read operations)
@@ -20,7 +20,7 @@ class RequestLoggingMiddleware:
             user = request.user if request.user.is_authenticated else None
             method = request.method
             endpoint = request.path
-            data_sent = str(self.body, 'utf-8') if self.body else ''
+            data_sent = self.extract_request_data(request)
             response_data = response.content.decode('utf-8')
 
             if method != 'GET' and method != 'OPTIONS' and not endpoint.startswith('/admin') and not endpoint.startswith('/api/auth/login') and not endpoint.startswith('/api/auth/refresh'):
@@ -34,3 +34,27 @@ class RequestLoggingMiddleware:
                 )
 
         return response
+    
+    def extract_request_data(self, request):
+        # Extract and return request data based on content type
+        content_type = request.content_type
+
+        if 'application/json' in content_type:
+            return str(request.body, 'utf-8') if request.body else ''
+        elif 'multipart/form-data' in content_type:
+            # Handle form data with images
+            form_data = {}
+            for key, value in request.POST.items():
+                form_data[key] = value
+
+            for key, file in request.FILES.items():
+                # Check if the file is an image
+                if file.content_type.startswith('image'):
+                    form_data[key] = f'Uploaded image: {file.name}'
+                else:
+                    # Handle other file types if needed
+                    form_data[key] = f'Uploaded file: {file.name}'
+
+            return json.dumps(form_data)
+
+        return ''
